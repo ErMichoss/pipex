02392e66bcc645b17fd001ce03a32f8acec4b115
Nicolas Gonzalez de Mendoza <73661812+ErMichoss@users.noreply.github.com>
2024-03-16 19:36:35 +0100

    FUMADA

--- src/ft_exc_cmd.c
+++ src/ft_exc_cmd.c
@@ -6,7 +6,7 @@
 /*   By: nicgonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 14:21:26 by nicgonza          #+#    #+#             */
-/*   Updated: 2024/03/16 16:08:27 by nicgonza         ###   ########.fr       */
+/*   Updated: 2024/03/16 19:34:56 by nicgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,25 @@ char	*get_comand(char **paths, char *comand)
 {
 	char	*tmp;
 	char	*exe;
+	int		i;
 
-	if ((comand[0] >= 'a' && comand[0] <= 'z')
-		|| (comand[0] >= 'A' && comand[0] <= 'Z')
-		|| (comand[0] >= '0' && comand[0] <= '9'))
+	while (comand[i] != '\0')
 	{
-		while (*paths)
-		{
-			tmp = ft_strjoin(*paths, "/");
-			exe = ft_strjoin(tmp, comand);
-			free(tmp);
-			if (access(exe, 0) == 0)
-				return (exe);
-			free(exe);
-			paths++;
-		}
+		if (comand[i] == '/')
+			return (comand);
+		i++;
 	}
-	return (NULL);
+	while (*paths)
+	{
+		tmp = ft_strjoin(*paths, "/");
+		exe = ft_strjoin(tmp, comand);
+		free(tmp);
+		if (access(exe, 0) == 0)
+			return (exe);
+		free(exe);
+		paths++;
+	}
+	return (comand);
 }
 
 void	ft_exec_cmd1(char **argv, char **envp, t_pipex pipex)
